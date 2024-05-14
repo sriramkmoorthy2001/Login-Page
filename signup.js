caefb1d40ee3d9972681b3209228dffda04d4f37
SRIRAM K MOORTHY <sriramkmoorthy2001@gmail.com>
2024-05-14 22:16:52 +0530

    Form validation

--- signup.js
+++ signup.js
@@ -58,3 +58,56 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+//Form Validation
+
+let input = document.querySelectorAll("input")
+let button = document.querySelector(".submit")
+let warning = document.getElementById("warning")
+let warning1 = document.getElementById("warning1")
+let warning2 = document.getElementById("warning2")
+let warning3 = document.getElementById("warning3")
+let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+function formvalidate() {
+  let username = input[0].value
+  let email = input[1].value 
+  let password = input[2].value
+
+  // Clear previous warnings
+  warning.innerText = "";
+  warning1.innerText = "";
+  warning2.innerText = "";
+  warning3.innerText = "";
+
+  if(username == "" || email == "" || password ==""){
+      warning.innerText = "PLEASE FILL THE REQUIRED FIELD"
+      warning.style.color = "red"
+      warning.style.textAlign = "center"
+  }
+  else if(username.length<5 || username.length>15) {
+      warning1.innerText = "NAME'S CHARACTER SHOULD BE BEWTEEN 5 AND 15"
+      warning1.style.color = "red"
+      warning1.style.marginTop = "3px"
+      warning1.style.fontSize = "10px";
+  }
+  else if(!emailRegex.test(email)) {
+        warning2.innerText = "PLEASE ENTER THE EMAIL"
+        warning2.style.color = "red"
+        warning2.style.marginTop = "3px"
+        warning2.style.fontSize = "10px";
+  }
+  else if(password.length<8 || password.length>15) {
+        warning3.innerText = "PASSWORD MUST BE BETWEEN 8 TO 15 CHARACTERS!!!"
+        warning3.style.color = "red"
+        warning3.style.marginTop = "3px"
+        warning3.style.fontSize = "10px"
+  }   
+  else {
+        warning.innerText = "";
+        warning1.innerText = "";
+        warning2.innerText = " ";
+        warning3.innerText = "";
+  }
+  console.log(...input)
+}
